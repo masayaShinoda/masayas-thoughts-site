03dc268b0fb7f837c4e223d1c28e53c715faf6e6
Masaya Shida <masayashida@gmail.com>
2021-11-12 21:00:21 +0700

    updated loader

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -73,18 +73,44 @@
         </section>
         {/each}
         {:else}
-        <div class="load-wrapp">
-            <div class="load-7">
-                <div class="square-holder">
-                <div class="square"></div>
-                </div>
-            </div>
-        </div>
+        <span class="loader"></span>
     {/if}
 </div>
 
 <style>
-    @import '/static/styles/loading-anim.css';
+    
+    .loader {
+        width: 48px;
+        height: 48px;
+        border: 3px solid #666;
+        border-bottom-color: transparent;
+        border-radius: 50%;
+        display: inline-block;
+        position: relative;
+        box-sizing: border-box;
+        animation: rotation 1s linear infinite;
+    }
+    .loader::after {
+        content: '';  
+        position: absolute;
+        box-sizing: border-box;
+        left: 20px;
+        top: 31px;
+        border: 10px solid transparent;
+        border-right-color: #666;
+        transform: rotate(-40deg);
+    }
+
+    @keyframes rotation {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    } 
+        
+
     .individual {padding: 1.8rem 0;}
     .individual:hover, .individual:focus {outline: 2px dashed #999; outline-offset: 1.8rem;}
     .blog_link {
