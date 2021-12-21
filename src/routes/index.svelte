dd258805d227ea9eaf22cb3727cf6d8d7376b984
Masaya Shida <masayashida@gmail.com>
2021-12-21 17:12:00 +0700

    seo image fix index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,13 +12,13 @@
     <meta property="og:type" content="website">
     <meta property="og:title" content="Masaya's Thoughts">
     <meta property="og:description" content="Expressing my thoughts and worldview as a Khmer-Japanese through writing.">
-    <meta property="og:image" content="static/images/masaya-celestial.jpg">
+    <meta property="og:image" content="/images/masaya-celestial.jpg">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:title" content="Masaya's Thoughts">
     <meta property="twitter:description" content="Expressing my thoughts and worldview as a Khmer-Japanese through writing.">
-    <meta property="twitter:image" content="static/images/masaya-celestial.jpg">
+    <meta property="twitter:image" content="/images/masaya-celestial.jpg">
 
 </svelte:head>
 <Hero />
